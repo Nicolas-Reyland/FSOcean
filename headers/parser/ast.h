2dc4c95a2407081962bd4b7713f9e42ce6e2b056
Nicolas Reyland <nicolas@reyland.dev>
2022-08-26 19:22:50 +0900

    removed old ast code

--- headers/parser/ast.h
+++ headers/parser/ast.h
@@ -6,54 +6,9 @@
 #define OCEAN_AST_H
 
 #include <stddef.h>
-#include "misc/stack.h"
 #include "cst.h"
+#include "executable/executable.h"
 
-typedef enum {
-    AST_NONE,
-    //
-    AST_COMMAND_LIST,
-
-    AST_COMMAND_SCOPE,
-    AST_COMMAND_CLASSIC,
-
-    AST_REDIRECTS,
-    AST_REDIRECT_IN,
-    AST_REDIRECT_OUT,
-
-    AST_NAMES,
-    AST_PREFIXES,
-    //
-    AST_IF_STATEMENT,
-
-    AST_FOR_LOOP,
-    AST_WHILE_LOOP,
-    AST_UNTIL_LOOP,
-
-    AST_CASE_STATEMENT,
-    AST_CASE_EXPR,
-    AST_CASE_MATCHES,
-
-    AST_CONDITIONAL_BRANCH,
-    //
-
-    AST_VALUE,
-    AST_EVAL_VALUE,
-
-    //
-    AST_EMPTY,
-} AbstractNodeType;
-
-typedef struct ASTNode {
-    AbstractNodeType type;
-    char * str;
-    size_t str_len;
-    struct ASTNode * children;
-    size_t num_children;
-} ASTNode;
-
-extern const char * const ABSTRACT_NODE_TYPE_STRING[];
-
-ASTNode abstract_cst(CSTNode cst_node);
+Executable abstract_cst(CSTNode cst_node);
 
 #endif // OCEAN_AST_H
