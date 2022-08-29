899a9d0712fff91c9b0f4ea692cbcbe13c0b6f80
Nicolas Reyland <nicolas@reyland.dev>
2022-08-29 12:30:52 +0200

    imperfect abstraction of compound list done

---
+++ headers/parser/abstractions/compound_command/imperfect_abstract_compound_list.h
@@ -0,0 +1,13 @@
+//
+// Created by Nicolas Reyland on 8/29/22.
+//
+
+#ifndef OCEAN_IMPERFECT_ABSTRACT_COMPOUND_LIST_H
+#define OCEAN_IMPERFECT_ABSTRACT_COMPOUND_LIST_H
+
+#include "executable/executable.h"
+#include "parser/cst.h"
+
+Executable * imperfect_abstract_compound_list(CSTNode compound_list, size_t * num_executables);
+
+#endif // OCEAN_IMPERFECT_ABSTRACT_COMPOUND_LIST_H
