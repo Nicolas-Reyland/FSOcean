0edd933c4ceed5283863f29e92099b008b6b7c3f
Nicolas Reyland <nicolas@reyland.dev>
2022-08-31 16:18:11 +0200

    implemented while loop abstraction

--- sources/parser/abstractions/compound_command/abstract_while_clause.c
+++ sources/parser/abstractions/compound_command/abstract_while_clause.c
@@ -3,8 +3,26 @@
 //
 
 #include "parser/abstractions/compound_command/abstract_while_clause.h"
-#include "misc/output.h"
+#include "parser/abstractions/abstraction.h"
+#include "parser/abstractions/compound_command/imperfect_abstract_compound_list.h"
 
 Executable abstract_while_clause(CSTNode while_clause) {
-    NOT_IMPLEMENTED_ERROR(abstract while clause)
+    NODE_COMPLIANCE(while_clause, WHILE_CLAUSE_PARSER, 3, GEN_STRING_R1_PARSER, COMPOUND_LIST_PARSER, DO_GROUP_PARSER)
+    CSTNode do_group = *while_clause.children[2];
+    NODE_COMPLIANCE(do_group, DO_GROUP_PARSER, 3, GEN_STRING_R1_PARSER, COMPOUND_LIST_PARSER, GEN_STRING_R1_PARSER)
+    size_t num_condition_executables = 0,
+           num_body_executables = 0;
+    Executable * condition_executables = imperfect_abstract_compound_list(*while_clause.children[1], &num_condition_executables),
+               * body_executables = imperfect_abstract_compound_list(*do_group.children[1], &num_body_executables);
+    return (Executable) {
+            .type = EXEC_WHILE_LOOP,
+            .executable = (union ExecutableUnion) {
+                    .while_loop = (struct ExecWhileLoop) {
+                            .num_condition_commands = num_condition_executables,
+                            .condition_commands = condition_executables,
+                            .body_commands = body_executables,
+                            .num_body_commands = num_body_executables,
+                    },
+            },
+    };
 }
